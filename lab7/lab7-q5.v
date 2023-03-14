beb2d2933b145aa8da048e8ea3574ffd736ccffe
dahu <dahu9049@colorado.edu>
2023-03-14 15:47:53 -0600

    work?

--- lab7/lab7-q5.v
+++ lab7/lab7-q5.v
@@ -9,30 +9,18 @@ module hexEncode(input [3:0] sw, output reg [7:0] D1_seg, D0_seg, output [3:0] D
             4'b0101 : D1_seg = 8'b10010010; // 5
             4'b0110 : D1_seg = 8'b10000010; // 6 
             4'b0111 : D1_seg = 8'b11111000; // 7
-            4'b1000 : begin D1_seg = 8'b10000000; // -8
-                            assign D1_seg[6] = 0;
-                        end
-            4'b1001 : begin D1_seg = 8'b11111000; // -7
-                            assign D1_seg[6] = 0;              
-                        end
-            4'b1010 : begin D1_seg = 8'b10000010; // -6
-                            assign D1_seg[6] = 0;
-                        end
-            4'b1011 : begin D1_seg = 8'b10010010; // -5
-                            assign D1_seg[6] = 0;
-                        end
-            4'b1100 : begin D1_seg = 8'b10011001; // -4
-                            assign D1_seg[6] = 0;
-                        end
-            4'b1101 : begin D1_seg = 8'b10110000; // -3
-                            assign D1_seg[6] = 0;
-                        end
-            4'b1110 :begin D1_seg = 8'b10100100; // -2
-                            assign D1_seg[6] = 0;
-                        end
-            4'b1111 :begin D1_seg = 8'b11111001; // -1
-                            assign D1_seg[6] = 0;
-                        end
+            4'b1000 : D1_seg = 8'b10000000; // -8
+            4'b1001 : D1_seg = 8'b11111000; // -7
+            4'b1010 : D1_seg = 8'b10000010; // -6
+            4'b1011 : D1_seg = 8'b10010010; // -5
+            4'b1100 : D1_seg = 8'b10011001; // -4
+            4'b1101 : D1_seg = 8'b10110000; // -3
+            4'b1110 : D1_seg = 8'b10100100; // -2
+            4'b1111 : D1_seg = 8'b11111001; // -1
+        endcase
+        casez(sw)
+            4'b1zzz : D0_seg = 8'b11011111; // negative
+            4'b0zzz : D0_seg = 8'b11111111; // positive
         endcase
     end
 
