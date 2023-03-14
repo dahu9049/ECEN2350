65ce0005bcdcd1410a15ae1fd9265529607a7eae
dahu <dahu9049@colorado.edu>
2023-03-14 15:12:33 -0600

    done

---
+++ lab7/negate.v
@@ -0,0 +1,10 @@
+module negate(input [3:0] in, output reg [3:0] out);
+    integer i;
+    always @* begin
+        for(i = 0; i < 4; i = i + 1) begin
+            if(in[i] == 1) out[i] = 1'b0;
+            else out[i] = 1'b1;
+            // #5
+        end
+    end
+endmodule
