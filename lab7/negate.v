a7b012343513f8d6cb490eb9ce87a0c3a58f425e
dahu <dahu9049@colorado.edu>
2023-03-15 13:28:52 -0600

    got rid of delay

--- lab7/negate.v
+++ lab7/negate.v
@@ -2,9 +2,9 @@ module negate(input [3:0] in, output reg [3:0] out);
     integer i;
     always @* begin
         for(i = 0; i < 4; i = i + 1) begin
-            if(in[i] == 1) out[i] = 1'b0;
-            else out[i] = 1'b1;
-            #5;
+            if(in[i] == 1) out[i] = 0;
+            else out[i] = 1;
+            
         end
     end
 endmodule
