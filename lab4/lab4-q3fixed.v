38e246fa2119cc266f7e8cab58e73b5a6756b3ee
dahu <dahu9049@colorado.edu>
2023-04-06 10:24:08 -0600

    both expressions work

--- lab4/lab4-q3fixed.v
+++ lab4/lab4-q3fixed.v
@@ -1,4 +1,4 @@
 module selector2(input a, b, s, output out);
-  // assign out = (b~&s)~&(~s~&a);
-  assign out = (a&~s) | (b&s);
+  assign out = (b~&s)~&(~s~&a); //both work
+  // assign out = (a&~s) | (b&s);
 endmodule
