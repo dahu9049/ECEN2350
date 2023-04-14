569525affa53ad7c29553b8a40acd2beeb49fb6e
dahu <dahu9049@colorado.edu>
2023-04-13 20:55:00 -0600

    done

---
+++ lab9/lab9-q4.v
@@ -0,0 +1,19 @@
+//Uses btn[0] and btn[1] as clocks
+//set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets btn[0]];
+//set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets btn[1]];
+
+module adder16(
+input [15:0] sw,
+input [3:0] btn,
+output [15:0] led
+);
+    reg [15:0] operand_a;
+    reg [15:0] operand_b;
+    reg [15:0] led_reg;
+    always @ (*) begin
+        if(btn[0]) operand_a <= sw;
+        if(btn[1]) operand_b <= sw;
+        led_reg <= operand_a + operand_b;
+    end
+    assign led = led_reg;
+endmodule
