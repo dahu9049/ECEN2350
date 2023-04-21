0ceadfd787b8fc07e85f2baba89422a03e4ea871
dahu <dahu9049@colorado.edu>
2023-04-20 19:44:02 -0600

    finished

--- lab10/lab10-q1.v
+++ lab10/lab10-q1.v
@@ -1,15 +1,15 @@
 module ClkDivider(
 input mclk,
-output led [15:0]
+output reg [15:0] led
 );
 reg [26:0] count = 0;
 always @(posedge mclk) begin
-if(count == 100000000 - 1) begin
-    count <= 0;
-    led <= -led
-end
-    else begin
-        count <= count + 1;
+    if(count == 100000000 - 1) begin
+        count <= 0;
+        led[0] <= ~led[0];
     end
+        else begin
+            count <= count + 1;
+        end
 end
 endmodule
